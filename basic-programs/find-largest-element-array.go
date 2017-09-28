61b200cfd606be293498de347ea01d818a18914f
Leonardo Souza <leogsouza@gmail.com>
2017-09-28 17:09:23 +0000

    Added Find Largest Element of an Array example

---
+++ basic-programs/find-largest-element-array.go
@@ -0,0 +1,26 @@
+// This program print the largest element of an Array
+
+package main
+
+import "fmt"
+
+func main() {
+	var num [100]float64
+	var temp int
+
+	fmt.Print("Enter number of elements: ")
+	fmt.Scanln(&temp)
+
+	for i := 0; i < temp; i++ {
+		fmt.Print("Enter the number: ")
+		fmt.Scanln(&num[i])
+	}
+
+	for j := 1; j < temp; j++ {
+		if num[0] < num[j] {
+			num[0] = num[j]
+		}
+	}
+
+	fmt.Print("The largest number is: ", num[0])
+}
