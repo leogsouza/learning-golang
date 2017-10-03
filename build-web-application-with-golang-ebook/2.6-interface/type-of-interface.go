7a629128367470da58905f990b5470177e9b608f
Leonardo Souza <leogsouza@gmail.com>
2017-10-03 17:25:40 +0000

    Added examples using interface

---
+++ build-web-application-with-golang-ebook/2.6-interface/type-of-interface.go
@@ -0,0 +1,63 @@
+package main
+
+type Human struct {
+	name  string
+	age   int
+	phone string
+}
+
+type Student struct {
+	Human
+	school string
+	loan   float32
+}
+
+type Employee struct {
+	Human
+	company string
+	money   float32
+}
+
+// define interfaces
+type Men interface {
+	SayHi()
+	Sing(lyrics string)
+	Guzzle(beerStein string)
+}
+
+type YoungChap interface {
+	SayHi()
+	Sing(lyrics string)
+	BorrowMoney(amount float32)
+}
+
+type ElderlyGent interface {
+	SayHi()
+	Sing(song string)
+	SpendSalary(amount float32)
+}
+
+func (h *Human) SayHi() {
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+}
+
+func (h *Human) Sing(lyrics string) {
+	fmt.Println("La la, la la la, la la la la...", lyrics)
+}
+
+func (h *Human) Guzzle(beerStein string) {
+	fmt.Println("Guzzle Guzzle Guzzle...", beerStein)
+}
+
+// Employee overloads SayHi
+func (e *Employee) SayHi() {
+	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name, e.company, e.phone)
+}
+
+func (s *Student) BorrowMoney(amount float32) {
+	s.loan += amount
+}
+
+func (e *Employee) SpendSalary(amount float32) {
+	e.money -= e.amount
+}
