eb99370411a7f21d48c2be61f137ea7788334312
Leonardo Souza <leogsouza@gmail.com>
2018-09-06 21:57:08 -0300

    Fix Binary-search

--- data-structures-algorithms/02-binary-search/02-binary-search.go
+++ data-structures-algorithms/02-binary-search/02-binary-search.go
@@ -25,6 +25,6 @@ func binarySearch(needle int, haystack []int) bool {
 }
 
 func main() {
-	items := []int{1, 2, 9, 20, 34, 44, 55, 76, 81}
-	fmt.Println(binarySearch(76, items))
+	items := []int{1, 2, 9, 20, 34, 44, 55, 76, 81, 83, 85, 90, 92, 94, 98, 100, 110, 150, 170, 200, 203, 215, 225, 231, 244, 250}
+	fmt.Println(newBinarySearch(250, items))
 }
